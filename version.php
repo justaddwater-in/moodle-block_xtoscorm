27204742fe4dfd115081bed7210bb6c39493598a
Himanshu <him.developer@gmail.com>
2026-05-08 21:14:38 +0530

    fix moodle requires

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component    = 'block_xtoscorm';
 $plugin->release      = '1.0';
 $plugin->version      = 2026042102;
-$plugin->supported    = [500, 501];
-$plugin->requires = 2022112800;
+$plugin->requires = 2022041900;
 $plugin->supported = [401, 500];
+$plugin->maturity = MATURITY_ALPHA;
